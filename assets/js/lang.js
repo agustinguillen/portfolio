f0ef4459e456bd40d7b0f4e785bacf7abfb27ed1
Agustin Guillen <agustinnguillen@gmail.com>
2021-04-19 15:38:53 -0300

    Commit inicial

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -2,9 +2,12 @@ $.getJSON("https://agustinguillen.github.io/portfolio/lang.json", function(json)
     //Lenguaje por defecto de la página sessionStorage.setItem("lang", "idioma")"
     
    $('#traducir').click(function(){
+
        console.log("al menos el boton xd");
-       let lang = $(this).attr('id');
-       $('lang').each(function(index, value){
+
+       var lang = $(this).attr('id');
+
+       $('.lang').each(function(index, value){
            $(this).text(json[lang][$(this).attr('key')])
        })
    })
